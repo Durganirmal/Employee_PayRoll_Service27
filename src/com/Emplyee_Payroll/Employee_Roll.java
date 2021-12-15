00294e823fff7a6b5a26d700622987f2af4b01ef
Durga Nirmal <durganirmal123@gmail.com>
2021-12-15 19:09:13 +0530

    Added :Welcome Message

---
+++ src/com/Emplyee_Payroll/Employee_Roll.java
@@ -0,0 +1,11 @@
+package com.Emplyee_Payroll;
+
+public class Employee_Roll {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee PayRoll Service");
+		
+
+	}
+
+}

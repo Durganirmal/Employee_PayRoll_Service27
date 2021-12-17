cd29fc599e774ce81a9dfb6524d3eb9c3b4b0c9d
Durga Nirmal <durganirmal123@gmail.com>
2021-12-17 09:09:59 +0530

    Added : Creating a Demo File

--- src/com/Emplyee_Payroll/EmployeePayrollMain.java
+++ src/com/Emplyee_Payroll/EmployeePayrollMain.java
@@ -3,8 +3,9 @@ package com.Emplyee_Payroll;
 public class EmployeePayrollMain {
 
 	 public static void main(String[] args) {
-		 Employee_Payroll_Service employeePayrollService = new Employee_Payroll_Service();
-	        employeePayrollService.readEmployeeDataFromConsole();
-	        employeePayrollService.writeEmployeeDataInConsole();
-	    }
-	}
+		 
+			 Employee_Payroll_Service employeePayrollService = new Employee_Payroll_Service();
+		        employeePayrollService.readEmployeeDataFromConsole();
+		        employeePayrollService.writeEmployeeDataInConsole();
+		    }
+		}
